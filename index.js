078304d42ff66ffec736e26f438d0c8e4d1b3fe0
Franziska Hinkelmann <franzih@chromium.org>
2018-05-22 20:45:47 -0400

    Initial import

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = 80;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
