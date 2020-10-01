07d0229ce46b19457193900a0473c3ff1b64ebf2
sarikaghanwat <61899304+sarikaghanwat@users.noreply.github.com>
2020-10-01 23:14:45 +0530

    Update index.js
    convert plain test response to html

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end('<html><body><h1>Hello, World!</h1></body></html>');
 
 });
 
