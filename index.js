3ff6807c06fd3b763ded0da5c9c092219d6f9f73
shivamchauhan-svg <67889440+shivamchauhan-svg@users.noreply.github.com>
2022-03-14 19:14:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 80;
+var port = 3000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
